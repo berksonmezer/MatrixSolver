9f55da7bb50e3636b0d083dd736faad645befa10
Berk Sonmezer <berk.sonmezer@ozu.edu.tr>
2020-12-16 18:36:47 +0300

    create petsc csr matrix and solve iteratively

--- MatrixSolver.py
+++ MatrixSolver.py
@@ -1,4 +1,4 @@
-import petsc, mpi4py
+
 import scipy.sparse as sparse
 from scipy.io import mmread
 import petsc4py
@@ -19,19 +19,22 @@ np_matrix = np.matrix([[1,2,1,4],
               [-3,1,3,2]])
 np_b= np.matrix([[13], [28], [20], [6]])
 
+csr_matrix = sparse.csr_matrix(np_matrix)
+indptr = csr_matrix.indptr
+indices = csr_matrix.indices
+data = csr_matrix.data
+csr = (indptr, indices, data)
+N = csr_matrix.shape[0]
+
 # Create a PETSc matrix
-A = PETSc.Mat().create(PETSc.COMM_WORLD)
-A.setSizes(np_matrix.shape[0], np_matrix.shape[1])
+A = PETSc.Mat().createAIJWithArrays(N, csr)
 A.setUp()
-for i in range(np_matrix.shape[0]):
-    for j in range(np_matrix.shape[1]):
-        A.setValue(i, j, np_matrix[i,j])
 
 A.assemblyBegin()
 A.assemblyEnd()
 
 print('Values of PETSc matrix:')
-print(A.getValues(range(np_matrix.shape[0]), range(np_matrix.shape[1])))
+print(A.getValues(range(csr_matrix.shape[0]), range(csr_matrix.shape[1])))
 
 # Create PETSc vectors
 x, b = A.createVecs()
@@ -42,10 +45,13 @@ for i in range(np_b.shape[0]):
 print('Values of PETSc vector b:')
 print(b.getValues(range(np_b.shape[0])))
 
-# create linear solver and solve
+# create an iterative solver and solve
 ksp = PETSc.KSP()
 ksp.create(PETSc.COMM_WORLD)
 ksp.setOperators(A)
+pc = ksp.getPC()
+pc.setType('jacobi')
+ksp.setTolerances(rtol=1.e-7)
 ksp.solve(b, x)
 
 print('The solution is:')
