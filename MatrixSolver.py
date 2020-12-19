30b37618a12e0a2c0c70a1e74b5cf99a5eb12021
Berk Sonmezer <berk.sonmezer@ozu.edu.tr>
2020-12-19 22:06:22 +0300

    solve a matrix iteratively in parallel

--- MatrixSolver.py
+++ MatrixSolver.py
@@ -1,4 +1,3 @@
-
 import scipy.sparse as sparse
 from scipy.io import mmread
 import petsc4py
@@ -6,53 +5,62 @@ petsc4py.init()
 from petsc4py import PETSc
 import numpy as np
 
-#matrix = mmread("bcsstk01.mtx")
-#csr_matrix = sparse.csr_matrix(matrix)
-#indices = csr_matrix.indices
-#indptr = csr_matrix.indptr
-#data = csr_matrix.data
-#N = csr_matrix.shape[0]
+# read mtx 
+matrix_name = "bcsstk06.mtx"
+matrix = mmread(matrix_name)
+matrix = matrix.toarray()
+N = matrix.shape[0]
 
-np_matrix = np.matrix([[1,2,1,4],
-              [2,0,4,3],
-              [4,2,2,1],
-              [-3,1,3,2]])
-np_b= np.matrix([[13], [28], [20], [6]])
+# create PETSc comm
+comm = PETSc.COMM_WORLD
+size = comm.getSize()
+rank = comm.getRank()
 
-csr_matrix = sparse.csr_matrix(np_matrix)
-indptr = csr_matrix.indptr
-indices = csr_matrix.indices
-data = csr_matrix.data
-csr = (indptr, indices, data)
-N = csr_matrix.shape[0]
+# create PETSc vectors
+x = PETSc.Vec().create(comm=comm)
+x.setSizes(N)
+x.setFromOptions()
 
-# Create a PETSc matrix
-A = PETSc.Mat().createAIJWithArrays(N, csr)
+b = x.duplicate()
+u = x.duplicate()
+
+rstart, rend = x.getOwnershipRange()
+nlocal = x.getLocalSize()
+
+# Create PETSc matrix
+A = PETSc.Mat().create(comm=comm)
+A.setSizes(N, nlocal)
+A.setFromOptions()
 A.setUp()
+
+print(rstart, rend)
+for i in range(rstart, rend):
+    for j in range(matrix.shape[1]):
+        A.setValue(i, j, matrix[i,j])
 
 A.assemblyBegin()
 A.assemblyEnd()
 
-print('Values of PETSc matrix:')
-print(A.getValues(range(csr_matrix.shape[0]), range(csr_matrix.shape[1])))
+# set PETSc vectors
+u.set(1.0)
+b = A(u)
 
-# Create PETSc vectors
-x, b = A.createVecs()
-
-for i in range(np_b.shape[0]):
-    b.setValue(i, np_b[i, 0])
-
-print('Values of PETSc vector b:')
-print(b.getValues(range(np_b.shape[0])))
-
-# create an iterative solver and solve
-ksp = PETSc.KSP()
-ksp.create(PETSc.COMM_WORLD)
+# create ksp solver and solve
+ksp = PETSc.KSP().create(comm=comm)
 ksp.setOperators(A)
 pc = ksp.getPC()
 pc.setType('jacobi')
 ksp.setTolerances(rtol=1.e-7)
+ksp.setFromOptions()
 ksp.solve(b, x)
 
-print('The solution is:')
-print(x.getValues(range(np_b.shape[0])))
+# check the error
+x = x - u # x.axpy(-1.0,u)
+norm = x.norm(PETSc.NormType.NORM_2)
+its = ksp.getIterationNumber()
+
+PETSc.Sys.Print("Norm of error {}, Iterations {}\n".format(norm,its),comm=comm)
+if size==1:
+    PETSc.Sys.Print("- Serial OK",comm=comm)
+else:
+    PETSc.Sys.Print("- Parallel OK",comm=comm)
